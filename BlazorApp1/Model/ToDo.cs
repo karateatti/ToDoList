c3fb52ced3132b323f369c32e4bbabce692af731
Attila Tóth <karateatti@gmail.com>
2024-12-07 15:27:30 +0100

    Add project files.

---
+++ BlazorApp1/Model/ToDo.cs
@@ -0,0 +1,23 @@
+﻿namespace BlazorApp1.Model
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public bool? IsDone { get; set; }
+        public string? Description { get; set; }
+
+        static public int idhelper = 0;
+
+        public ToDo ()
+        {
+            Id = idhelper;
+            idhelper++;
+        }
+    }
+
+
+
+
+    
+}
---
+++ BlazorApp1/Model/ToDoList.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Model
+{
+    public class ToDoList
+    {
+        public List<ToDo>? ToDos { get; set; } = [];
+
+        public ToDoList() 
+        {
+            ToDos = new List<ToDo>();
+        }
+
+        public async Task LoadFile(HttpClient http)
+        {
+            string json = await http.GetStringAsync("JsonInput.json");
+            var options = new JsonSerializerOptions()
+            {
+                AllowTrailingCommas = true
+            };
+            if (json != null)
+            {
+                ToDoList root = JsonSerializer.Deserialize<ToDoList>(json, options);
+
+                ToDos = root.ToDos;
+            }
+        }
+    }
+
+    
+}
